57152c58861898447db9f223ad9e2219504e95e2
Lexi Schindel <lchasney@flyhomes.com>
2021-06-30 13:38:15 -0700

    setup react for server.js

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const port = process.env.PORT || 3001;
 const path = require("path");
 // const routes = require("./routes");
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 // app.use(routes);
 
 // create a GET route
 app.get('/connectToServer', (req, res) => {
   res.send({ express: 'The backend is connect to the frontend' });
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
 });
 
 // start server
