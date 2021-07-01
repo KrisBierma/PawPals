6c07b1755001036976728abe8d6a31ed99221528
Liv B <32025316+livbar@users.noreply.github.com>
2021-06-30 19:35:58 -0500

    removed import Header line
    import Header from.... line not needed for BrowsePage.jsx and FavoritesPage.jsx

--- client/src/pages/BrowsePage/BrowsePage.jsx
+++ client/src/pages/BrowsePage/BrowsePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Header from '../../components/Header'
 
 export default function BrowsePage() {
     return "Browse Page";
