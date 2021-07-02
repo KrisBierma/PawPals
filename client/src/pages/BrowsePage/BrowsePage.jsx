6e57fbc2636e4a0e0f9231abaea39ec2c87bcc97
Liv B <32025316+livbar@users.noreply.github.com>
2021-07-02 16:59:05 -0500

    Set up routes for Browse, Favorites and News
    Set up routes for Browse, Favorites and News
    
    Added News page and corresponding index.js and .jsx files

--- client/src/pages/BrowsePage/BrowsePage.jsx
+++ client/src/pages/BrowsePage/BrowsePage.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 
 export default function BrowsePage() {
-    return "Browse Page";
+    return "Hello, I am the BROWSE Page";
 }
---
+++ client/src/pages/NewsPage/index.js
@@ -0,0 +1,3 @@
+import NewsPage from 'pages/NewsPage/NewsPage';
+
+export default NewsPage;
