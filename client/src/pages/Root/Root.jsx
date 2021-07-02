170e6a143622f400b160025cbc0971783ccd63f4
Liv B <32025316+livbar@users.noreply.github.com>
2021-07-02 17:43:58 -0500

    updated import statement for Route
    warning was showing up in Chrome console related to importing Route. Updated per:
    https://reactrouter.com/web/api/Router

--- client/src/pages/Root/Root.jsx
+++ client/src/pages/Root/Root.jsx
@@ -4,9 +4,8 @@ import LandingPage from '../LandingPage/LandingPage'
 import BrowsePage from '../BrowsePage/BrowsePage'
 import FavoritesPage from '../FavoritesPage/FavoritesPage'
 import NewsPage from '../NewsPage/NewsPage'
-
-import { BrowserRouter as Router} from 'react-router-dom';
-import Route from 'react-router-dom/Route';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from "react-router";
 
 // Todo: set this up to pass in currently selected page
 
