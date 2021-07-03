77542530f9903a8f3c49d226a2de6257f193b4d3
Lexi Schindel <lchasney@flyhomes.com>
2021-07-03 14:38:36 -0700

    fix pages file structure and remove root

--- client/src/pages/FavoritesPage/FavoritesPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-
-
-export default function FavoritesPage() {
-    return "Hello I am the FAVORITES Page";
-}
