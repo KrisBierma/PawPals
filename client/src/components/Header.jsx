69ddb45daf4ea4ec532862301b53cc11c23797d8
Lexi Schindel <lchasney@flyhomes.com>
2021-07-06 12:15:54 -0700

    tab styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "../styles/Header.css";
-import { BasicModal } from "../components/Common";
+import LoginSignUp from "./LoginSignUp"
 
 // TODO: add conditionals for if logged in, highlight selected tab, add login functionality
 // TODO: pull user name from database
@@ -26,30 +26,7 @@ const headersData = [
     },
 ];
 
-const loginFields = [
-    {
-        name: "Username",
-        type: "input",
-    },
-    {
-        name: "Password",
-        type: "input",
-    }
-]
-
-// handles the login button click
-const handleLogin = (setLoginModalOpen) => {
-    setLoginModalOpen(true);
-}
-
-// handles modal close; passed to common modal component
-const handleLoginClose = (setLoginModalOpen) => {
-    setLoginModalOpen(false);
-}
-
 export default function Header(props) {
-    const [loginModalOpen, setLoginModalOpen] = useState(false); 
-
     // Currently our only display; creates full navbar
     const displayDesktop = () => {
         return (
@@ -73,18 +50,7 @@ export default function Header(props) {
     );
 
     const login = (
-        <>
-            <BasicModal 
-                show={loginModalOpen}
-                handleClose={() => handleLoginClose(setLoginModalOpen)}
-                title={'Member Login'}
-                saveTitle={'Login'}
-                fields={loginFields}
-            />
-            <Nav className="ml-auto">
-                <Button variant="primary" onClick={() => handleLogin(setLoginModalOpen)}>Login</Button>
-            </Nav>
-        </>
+        <LoginSignUp />
     );
 
     const settingDropDown = (
