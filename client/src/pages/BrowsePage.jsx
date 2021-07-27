7b3f7a97be924c454f759bae283d6b51be4df20a
LB <lilu99@gmail.com>
2021-07-27 12:10:04 -0500

    updated search functionality

--- client/src/pages/BrowsePage.jsx
+++ client/src/pages/BrowsePage.jsx
@@ -2,12 +2,21 @@ import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from '../components/AuthContext';
 import { GridLayout } from "../components/Common"
 import axios from 'axios';
+import SearchFilter from "../components/Common/SearchFilter";
+
 
 export default function BrowsePage() {
     const [animals, setAnimals] = useState([]);
+    const [userID, setUserID] = useState(null);
     const [availabilities, setAvailabilities] = useState(1);
-    const [breeds, setBreeds] = useState(1);
+    const [breeds, setBreeds] = useState([]);
     const context = useContext(AuthContext);
+    // const { enqueueSnackbar } = useSnackbar();
+    const [filterOption, setFilterOption] = useState({
+        atype: "",
+        breed: "",
+        gender: "",
+    });
 
     useEffect(() => {
         getAnimals();
@@ -24,16 +33,40 @@ export default function BrowsePage() {
 // routes to look for that api 
 // controller with direction for the db
 // db and get data
-
-    function getAnimals() {
-        // send -1 if no user bc postgres can't handle null for an int
-        var userIdToString = context.userID === null ? -1 : context.userID;
-        axios.get(`/api/getAnimalsWiFavs/${userIdToString}`)
-        .then(response => {
-            setAnimals(response.data);
-            // console.log(response.data)
-        }).catch(err =>console.log(err));
+const getBreeds = async (atype) => {
+    try {
+        const response = await axios.get(`/api/getBreeds/${atype}`);
+        console.log('setBreeds', response.data);
+        setBreeds(response.data);
+    } catch (error) {
+        console.log(error);
     }
+};
+
+const getAnimals = async (atype, gender, breed) => {
+    axios
+        .get(`/api/getAnimalsWiFavs`, {
+            params: {
+                userID: context.userID,
+                atype,
+                gender,
+                breed,
+            },
+        })
+        .then((response) => {
+            console.log(response.data);
+            setAnimals(response.data);
+        })
+        .catch((err) => console.log(err));
+};
+
+    useEffect(() => {
+        getAnimals(filterOption.atype, filterOption.gender, filterOption.breed);
+    }, [filterOption.atype, filterOption.gender, filterOption.breed]);
+
+    useEffect(() => {
+        if (filterOption.atype) getBreeds(filterOption.atype);
+    }, [filterOption.atype]);
 
     // for populating the dropdown menu; use id as key 
     function getDropdownInfo() {
@@ -49,10 +82,15 @@ export default function BrowsePage() {
         })
     }
 
+    const onChangeFilter = (e) => {
+        setFilterOption({ ...filterOption, [e.target.name]: e.target.value });
+    };
+
     return (
         <div>
             {/* breeds dropdown  */}
             { animals.length === 0 ? <p>All our animals currently have homes!</p> : <p></p>}
+            <SearchFilter onChange={onChangeFilter} breeds={breeds} />
             <GridLayout cardData={animals} />
         </div>
     )
